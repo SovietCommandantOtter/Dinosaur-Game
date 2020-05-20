e27f5b603f8e56efe0bedaa70c6799fea6c49dae
Jack Pirone <jackpirone21@winchendon.org>
2020-05-20 11:36:59 -0400

    .gitignore updates

---
+++ DinosaurGame/src/xyz/sovietcommandantotter/dinosaurgame/Game.java
@@ -0,0 +1,21 @@
+package xyz.sovietcommandantotter.dinosaurgame;
+
+import javax.swing.JFrame;
+
+public class Game {
+	private static final String APPLICATION_NAME = "Jumpy Dinosaur";
+	private static final int NORMAL_WIDTH = 960;
+	private static final int NORMAL_HEIGHT = 540;
+
+	public static JFrame window = new JFrame(APPLICATION_NAME);
+
+	public static void main(String[] args) {
+		window.setSize(NORMAL_WIDTH, NORMAL_HEIGHT);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLayout(null);
+		
+		
+		
+		window.setVisible(true);
+	}
+}
