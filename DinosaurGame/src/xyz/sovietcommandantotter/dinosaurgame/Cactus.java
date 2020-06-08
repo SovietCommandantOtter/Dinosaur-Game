1002e132bfc3103b1483d9581abf59b2daf33c3c
Jack Pirone <jackpirone21@winchendon.org>
2020-06-08 11:16:56 -0400

    Cactus Overhaul

--- DinosaurGame/src/xyz/sovietcommandantotter/dinosaurgame/Cactus.java
+++ DinosaurGame/src/xyz/sovietcommandantotter/dinosaurgame/Cactus.java
@@ -1,25 +1,33 @@
 package xyz.sovietcommandantotter.dinosaurgame;
 
-import java.awt.Color;
+import java.awt.Window;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Cactus extends JLabel {
+public class Cactus implements Runnable {
 
-	private int x;
-	private int y;
+	Window cactusWindow;
+	private static ImageIcon cactusImage = new ImageIcon(
+			"src/xyz/sovietcommandantotter/dinosaurgame/resources/graphics/small_cactus.png");
+	private static JLabel cactusLabel = new JLabel(cactusImage);
 
-	public Cactus() {
-		this.setSize(150, 150);
-		this.setLocation(10, 10);
-		this.setBackground(Color.BLUE);
-		this.setOpaque(true);
-		this.setVisible(true);
+	public Cactus(Window window) {
+		this.cactusWindow = window;
+		window.add(cactusLabel);
 	}
 
+	public void run() {
+		try {
+			while (true) {
+				Thread.sleep(10);
+				cactusLabel.setLocation(cactusLabel.getX() - 1, 50);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	public void addCactus(int x, int y) {
-		this.x = x;
-		this.y = y;
 	}
+
 }
--- DinosaurGame/src/xyz/sovietcommandantotter/dinosaurgame/Game.java
+++ DinosaurGame/src/xyz/sovietcommandantotter/dinosaurgame/Game.java
@@ -1,38 +1,42 @@
 package xyz.sovietcommandantotter.dinosaurgame;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 
-
-
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import xyz.sovietcommandantotter.dinosaurgame.Cactus;
 
 public class Game {
 	private static final String APPLICATION_NAME = "Jumpy Dinosaur";
 	private static final int NORMAL_WIDTH = 960;
-	private static final int NORMAL_HEIGHT = 540;
+	private static final int NORMAL_HEIGHT = 300;
 
 	public static JFrame window = new JFrame(APPLICATION_NAME);
-	public static Cactus cactus = new Cactus();
-	public static ScrollingBackground background = new ScrollingBackground();
-	public static JLabel testCactus = new JLabel("cactus");
+	
+	private static ImageIcon cactusImage = new ImageIcon("src/xyz/sovietcommandantotter/dinosaurgame/resources/graphics/small_cactus.png");
+	private static JLabel cactusLabel = new JLabel(cactusImage);
+	
+	private static Cactus cactus = new Cactus(window);
 	
 	public static void main(String[] args) {
+		
 		window.setSize(NORMAL_WIDTH, NORMAL_HEIGHT);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocationRelativeTo(null);
+		window.setResizable(false);
 		
 		
-		//((Component)background).setFocusable(true);
-		window.add(background);
-		background.setVisible(true);
-		//window.add(cactus);
-		
-		//testCactus.setBounds(150, 150, 150, 150);
-		//window.add(testCactus);
-		
+		//window.add(cactusLabel);
+		//cactusLabel.setLocation(500, 50);
+		//cactusLabel.setSize(cactusImage.getIconWidth(), cactusImage.getIconHeight());
 		window.setVisible(true);
+		cactus.run();
 		
+		
+		
+
 	}
 }
